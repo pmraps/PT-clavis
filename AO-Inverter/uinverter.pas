865ae68640755e499fca5a09209496e79b62f945
Pedro <pmra@protonmail.com>
2020-12-13 17:47:39 +0000

    Actualizar aoinverter.ico, aoinverter.lpi e mais 11 ficheiros...

--- AO-Inverter/uinverter.pas
+++ AO-Inverter/uinverter.pas
@@ -5,11 +5,7 @@ unit uinverter ;
 interface
 
 uses
-  Crt, Classes , SysUtils , Forms , Controls , Graphics , Dialogs , ExtCtrls , Menus ,
-  ComCtrls , StdCtrls ;
-
-const
-  ENTER = #13;
+  Classes , SysUtils , Forms , Controls , Graphics , Dialogs , StdCtrls ;
 
 type
 
@@ -17,15 +13,18 @@ type
 
   TForm1 = class(TForm )
     Button1 : TButton ;
-    Image1 : TImage ;
-    Menu : TMainMenu ;
-    ToolBar1 : TToolBar ;
     procedure Button1Click (Sender : TObject );
   private
 
   public
 
   end;
+
+resourcestring
+  ExitCaption = 'Quit the program';
+  ExitMsg 	  = 'Are You sure you want to exit the application?';
+  YButton	  = 'Yes';
+  NButton	  = 'No';
 
 var
   Form1 : TForm1 ;
@@ -38,7 +37,10 @@ implementation
 
 procedure TForm1 .Button1Click (Sender : TObject );
 begin
-  Halt;
+  Button1.Caption := ExitCaption;
+  if MessageDlg (ExitCaption, ExitMsg, mtConfirmation,
+                  [mbYes, mbNo],0) = mrYes
+  then Halt;
 end;
 
 end.
